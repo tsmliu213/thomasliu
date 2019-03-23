ded52e9066936e3b8814483a3c4fb5ff631aab44
tsmliu213 <tsmliu213@gmail.com>
2019-03-23 12:03:55 -0700

    chore: more cleanup and setup homepage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Thomas Liu`,
     author: `Thomas Liu`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `http://thomasliu.com`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -63,9 +63,7 @@ class Layout extends React.Component {
         <header>{header}</header>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          © {new Date().getFullYear()} Last update: 12/27/16
         </footer>
       </div>
     )
